fed3d864cb94175b243a74ee356af687d66b56a3
Erick Estrada <erickdallas30@gmail.com>
2022-12-15 01:08:21 -0600

    deploy lab 2 commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static('public'))
+// app.use(express.static('${__dirname}/public))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+// app.get('/styles')
+
+
+
+app.listen(4000, () => console.log('gliding on 4000'))
